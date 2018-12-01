a6263adee8dd59654c917adc395db5a8e36dce93
yanxianUCSB <yanxian@umail.ucsb.edu>
2018-12-01 14:21:45 -0800

    generate testcases with max flow computed

--- generate_testcases.m
+++ generate_testcases.m
@@ -1,29 +1,32 @@
 clear all;
 size_seq = 20 * [10, 20, 40];  % size of the matrix
-a_seq = 1:3;    % number of sources
-b_seq = 1:3;    % number of sinks
-p_seq = [0.5 0.6 0.7];      % density of graph (to be explained)
-r_seq = 1;      % nunmber of trauma (to be explained)
+a_seq = [10, 20, 40];    % number of sources
+b_seq = [10, 20, 40];    % number of sinks
+p_seq = [0.2, 0.5, 0.8];      % density of graph (to be explained)
+r_seq = 10;      % nunmber of trauma (to be explained)
 
 rng(1000);  % set seeds
 
 allComb = allcomb(size_seq, a_seq, b_seq, p_seq, r_seq);
 nRow = nrow(allComb);
-for iRow = 1:1
+for iRow = 1:nRow
 %     disp(iRow)
     s = round(allComb(iRow, 1));
     a = round(allComb(iRow, 2));
     b = round(allComb(iRow, 3));
     p = (allComb(iRow, 4));
     r = round(allComb(iRow, 5));
-    [A,B]=createadjmat(s,a,b,p,r);
-     disp(size(A))
-     disp(size(B))
+    [A,B, maxFlowBefore, maxFlowAfter]=createadjmat(s,a,b,p,r);
+%      disp(size(A))
+%      disp(size(B))
+    disp(strcat(num2str(maxFlowBefore), '\t', num2str(maxFlowAfter)))
     if isSquare(A) && isSquare(B)
         csvwrite(strcat("testcases/linear/adjm_A_", num2str(iRow), ".csv"), A);
         csvwrite(strcat("testcases/linear/adjm_B_", num2str(iRow), ".csv"), B);
     end
+    allComb(iRow, 6:7) = [maxFlowBefore, maxFlowAfter];
 end
+%%
 csvwrite("testcases/linear/allComb.csv", allComb);
 %%
 rng(1000);  % set seeds
