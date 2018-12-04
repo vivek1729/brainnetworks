14e3214e00ef4e6035a12d6c5c3c93d7eed0bdd9
yanxianUCSB <yanxian@umail.ucsb.edu>
2018-12-04 11:30:46 -0800

    write maxflow to the testcases

--- generate_testcases.m
+++ generate_testcases.m
@@ -1,9 +1,14 @@
 clear all;
-size_seq = 20 * [10, 20, 40];  % size of the matrix
+%x axises
+size_seq = 10 * [10, 20, 40];  % size of the matrix
 a_seq = [10, 20, 40];    % number of sources
 b_seq = [10, 20, 40];    % number of sinks
 p_seq = [0.2, 0.5, 0.8];      % density of graph (to be explained)
 r_seq = 10;      % nunmber of trauma (to be explained)
+
+%y axises
+%time
+%cost 
 
 rng(1000);  % set seeds
 
@@ -16,22 +21,26 @@ for iRow = 1:nRow
     b = round(allComb(iRow, 3));
     p = (allComb(iRow, 4));
     r = round(allComb(iRow, 5));
+    
+    disp([s, a, b, p, r]);
+    
     [A,B, maxFlowBefore, maxFlowAfter]=createadjmat(s,a,b,p,r);
 %      disp(size(A))
 %      disp(size(B))
     disp(strcat(num2str(maxFlowBefore), '\t', num2str(maxFlowAfter)))
+    
     if isSquare(A) && isSquare(B)
         csvwrite(strcat("testcases/linear/adjm_A_", num2str(iRow), ".csv"), A);
         csvwrite(strcat("testcases/linear/adjm_B_", num2str(iRow), ".csv"), B);
     end
     
-    %allComb(iRow, 6:7) = [maxFlowBefore, maxFlowAfter];
+    allComb(iRow, 6:7) = [maxFlowBefore, maxFlowAfter];
 end
 %%
 csvwrite("testcases/linear/allComb.csv", allComb);
 %%
 rng(1000);  % set seeds
-[A, B, f1, f2] = createadjmat(100, 1, 1, 1, 10000);
+[A, B, f1, f2] = createadjmat(200, 10, 10, 0.2, 10);
 size(A)
 size(B)
 f1, f2
