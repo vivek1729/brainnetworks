e791eaf16188c5c34f3fc46d4e5f814bf3da0910
yanxianUCSB <yanxian@umail.ucsb.edu>
2018-12-04 23:36:45 -0800

    plot for linear cost function generated

--- testcases/plot_results_linear_costFunc.r
+++ testcases/plot_results_linear_costFunc.r
@@ -2,7 +2,8 @@ library(ggplot2)
 library(ggsci)
 library(dplyr)
 source("~/Box/R/template.r")
-read.csv("linear/allComb_out.csv", header = F) %>% 
+readdf <- function(filename){
+read.csv(filename, header = F) %>% 
     rename(
         size.of.graph = V1,
         size.of.source = V2,
@@ -15,7 +16,10 @@ read.csv("linear/allComb_out.csv", header = F) %>%
         cost.after     = V9,
         cost.time = V10
     ) ->df
+    return(df)
+}
 
+df <- readdf("linear/allComb_out.csv")
 df %>% 
     summary
 
@@ -41,3 +45,34 @@ df %>%
     scale_color_npg2() +
     theme(legend.position = c(0.2,0.8)) +
     plot_save_pub('costtime-per-numberofnodes')
+
+#sizeofgraphset2 Tue Dec  4 13:11:15 2018 ------------------------------
+df <- readdf('sizeofgraphset2/allComb_out.csv')
+
+df %>% 
+    summary
+
+# Tue Dec  4 12:24:46 2018 ------------------------------
+#' size cost
+df %>% 
+    filter(size.of.graph < 1000) %>% 
+    # filter(size.of.source == 16) %>% 
+    # filter(size.of.sink == 16) %>% 
+    # filter(size.of.trauma == 32) %>%
+    mutate(
+        # x = size.of.trauma,
+        # y = (cost.after-cost.before) / (max.flow.before - max.flow.after)
+        x = size.of.graph,
+        y = cost.time,
+        group = naturalsort::naturalfactor(density.of.graph)
+    ) %>% 
+    plot_simple_points2(
+        'number of nodes',
+        'time cost',
+        'density of graph'
+    )+
+    geom_line(aes(color = group)) +
+    scale_color_npg2() +
+    theme(legend.position = c(0.2,0.8)) +
+    plot_save_pub('costtime-per-numberofnodes')
+
